5808178820839ab4815397208fbabb8e2ab9d680
hipotato <guanghuiemail@163.com>
2018-04-02 15:42:57 +0800

    “我的gitHub第一次提交”

---
+++ src/main/java/org/potato/AnyThing/AnyThingApplication.java
@@ -0,0 +1,12 @@
+package org.potato.AnyThing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyThingApplication.class, args);
+	}
+}
